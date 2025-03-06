a96df4dd53c0003904662fb7821874f4c581df47
vighnesh-kargutkar <kargutkar860@gmail.com>
2025-03-06 10:56:54 +0530

    final

--- src/app/counter/counter.component.ts
+++ src/app/counter/counter.component.ts
@@ -1,4 +1,9 @@
-import { Component, inject, NgZone, OnInit, signal } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  OnInit,
+  signal,
+} from '@angular/core';
 
 import { InfoMessageComponent } from '../info-message/info-message.component';
 
@@ -8,25 +13,24 @@ import { InfoMessageComponent } from '../info-message/info-message.component';
   templateUrl: './counter.component.html',
   styleUrl: './counter.component.css',
   imports: [InfoMessageComponent],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class CounterComponent implements OnInit{
-  private zone = inject(NgZone)
+export class CounterComponent implements OnInit {
   count = signal(0);
-
-  ngOnInit(): void {
-    setTimeout(()=>{
-      console.log("settimeout 1");
-    },4000)
-    this.zone.runOutsideAngular(()=>{
-      setTimeout(()=>{
-        console.log("settimeout 2");
-      },5000)
-    })
-  }
 
   get debugOutput() {
     console.log('[Counter] "debugOutput" binding re-evaluated.');
     return 'Counter Component Debug Output';
+  }
+
+  ngOnInit() {
+    setTimeout(() => {
+      this.count.set(0);
+    }, 4000);
+
+    setTimeout(() => {
+      console.log('Timer expired!');
+    }, 5000);
   }
 
   onDecrement() {
